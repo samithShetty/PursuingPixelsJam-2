89d735fac6b086cc93b7515d54d0673f6ffae4b2
Samith Shetty <71335825+samithShetty@users.noreply.github.com>
2023-05-17 13:34:53 -0400

    Added Player Control

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,33 +1,59 @@
 extends Node2D
 
-@export var moveSpeed = 5
-@export var maxHealth = 5
-@export var maxHunger = 5
-@export var maxEnergy = 5
+@export var moveSpeed: float = 5
+@export var maxHealth: float = 5
+@export var maxHunger: float = 5
+@export var maxEnergy: float = 5
 
-var health = 4
-var hunger = 5
-var energy = 5
+var anim: AnimationPlayer;
+var sprite: Sprite2D;
+
+var health = 4;
+var hunger = 5;
+var energy = 5;
+var jumping: bool = false;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	anim = $AnimationPlayer
+	sprite = $KnightSpritesheet
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
+	## PLAYER MOVEMENT
+	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
+		sprite.flip_h = false
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
+		sprite.flip_h = true
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
 	position += velocity.normalized() * moveSpeed;
 	
-
+	## ANIMATION HANDLING
+	if jumping:
+		anim.play("jump")
+	elif velocity != Vector2.ZERO:
+		anim.play("run")
+	else:
+		anim.play("idle")
+	
+func _unhandled_key_input(event):
+	if event.is_action_pressed("jump"):
+		anim.play("jump");
+		jumping = true;
+		scale = Vector2(1.5,1.5)
+	
+func end_jump():
+	jumping = false;
+	scale = Vector2(1,1)
+	
+	
 func update_health(delta_health): # Negative to damage player
 	health += delta_health
 	
