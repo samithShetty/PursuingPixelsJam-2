df3629ce32f9cd06ca84b81bdb52b7ac7d75ac41
Samith Shetty <71335825+samithShetty@users.noreply.github.com>
2023-05-19 15:36:57 -0400

    Implemented Start and Death Screens

---
+++ Screens+UI/Menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@export var MainScene: Resource
+@export var StartScene: Resource
+
+func _start_button_pressed():
+	get_tree().change_scene_to_packed(MainScene)
+
+
+func _quit_button_pressed():
+	get_tree().quit()
+
+
+
+func _return_button_pressed():
+	get_tree().change_scene_to_packed(StartScene)
